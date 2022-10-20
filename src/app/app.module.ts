a3365c369595bf31a0b3f788ed2a71c560be567d
Sameer Kumar <kumar.sam1612@gmail.com>
2022-10-20 13:27:25 +0530

    split application in components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { BlogComponent } from './blog/blog.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { FactsComponent } from './facts/facts.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ResumeComponent,
+    BlogComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    FactsComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    HeroComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
