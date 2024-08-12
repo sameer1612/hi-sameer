fd2b82ec47b86a37677c7f14978c8cfbc58adb69
Sameer Kumar <kumar.sam1612@gmail.com>
2024-08-12 16:33:17 +0530

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#025a4e',
+        secondary: '#547e78',
+        surface: '#e2e2e2'
+      }
+    }
+  },
+  plugins: []
+};
