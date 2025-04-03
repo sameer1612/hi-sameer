ea0ae39fa3030cad13e8e63cb8450d92a39579ec
Sameer Kumar <kumar.sam1612@gmail.com>
2025-04-03 15:11:39 +0530

    added homepage and setup nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
