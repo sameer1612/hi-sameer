0e0ef1d51f696ddfc74535adb5f194eb48fa6050
Sameer Kumar <kumar.sam1612@gmail.com>
2025-06-13 11:01:02 +0530

    added blog page

---
+++ src/types/blog.ts
@@ -0,0 +1,40 @@
+export type BlogUser = {
+  name: string;
+  username: string;
+  twitter_username: string | null;
+  github_username: string | null;
+  user_id: number;
+  website_url: string | null;
+  profile_image: string;
+  profile_image_90: string;
+};
+
+export type BlogArticle = {
+  type_of: string;
+  id: number;
+  title: string;
+  description: string;
+  readable_publish_date: string;
+  slug: string;
+  path: string;
+  url: string;
+  comments_count: number;
+  public_reactions_count: number;
+  collection_id: number | null;
+  published_timestamp: string;
+  language: string;
+  subforem_id: number | null;
+  positive_reactions_count: number;
+  cover_image: string | null;
+  social_image: string;
+  canonical_url: string;
+  created_at: string;
+  edited_at: string | null;
+  crossposted_at: string | null;
+  published_at: string;
+  last_comment_at: string;
+  reading_time_minutes: number;
+  tag_list: string[];
+  tags: string;
+  user: BlogUser;
+};
